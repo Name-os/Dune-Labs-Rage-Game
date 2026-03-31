354071d598353d61d117521ff0ad5349ec900c48
Fra <Francis.xun.su@gmail.com>
2026-03-31 13:38:16 +0800

    Fixed some stuff climbing and crouching

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,6 +18,7 @@ var stamina_cost_wall_jump := 40.0
 #state
 var crouching           := false
 var climbing            := false
+var climbing_moving     := false
 var allow_input         := true
 var on_floor_last_frame := false
 
@@ -38,19 +39,20 @@ func check_bound(lower, upper, num, equal=false):
 
 func animate():
 	$AnimatedSprite2D.flip_h = dir.x < 0 if dir.x != 0 else $AnimatedSprite2D.flip_h
-
 	var animation = "sleeping" if $timers/sleep.is_stopped() else \
 					"sitting"  if $timers/sit.is_stopped()   else \
 					("crouching_running" if dir.x else "crouching_idle") if crouching else \
 					"climbing" if climbing else \
 					("in_air_up" if velocity.y < 1 else "in_air_down") if not is_on_floor() else \
 					("running" if dir.x else "idle")
-
 	$AnimatedSprite2D.play(animation)
 
 func toggle_crouch():
-	$head.disabled = crouching
-	speed_mod = mod_values["crouching"] if crouching else 1.0
+	$head.disabled = not crouching
+	if crouching:
+		speed_mod = mod_values["crouching"]
+	elif not climbing:
+		speed_mod = 1.0
 	if dir.x != 0:
 		$head.position.x = -1.0 if dir.x < 1 else 3.0
 
@@ -63,7 +65,7 @@ func update_stamina(delta):
 	if is_on_floor() and on_floor_last_frame:
 		stamina = 100.0
 	elif climbing:
-		stamina = max(0, stamina - (stamina_drain_climb if dir.y else stamina_drain_grip) * delta)
+		stamina = max(0, stamina - (stamina_drain_climb if climbing_moving else stamina_drain_grip) * delta)
 
 func get_input_map() -> Dictionary:
 	return {
@@ -75,12 +77,14 @@ func get_input_map() -> Dictionary:
 
 func new_input(_delta):
 	gravity_mod = 1.0
+	climbing_moving = false
 	if not is_on_floor():
 		crouching = false
 	if wall_jump_cooldown <= 0:
 		climbing = false
 
-	dir = Input.get_vector("left", "right", "up", "down")
+	dir.x = Input.get_axis("left", "right")
+	dir.y = Input.get_axis("up", "down")
 	var im = get_input_map()
 
 	if im["climb"] and $body/ShapeCast2D.is_colliding() and wall_jump_cooldown <= 0:
@@ -92,25 +96,22 @@ func new_input(_delta):
 				climbing = false
 				wall_jump_cooldown = 15
 				if dir.x:
-					# jump away - no stamina cost
 					velocity = Vector2(-dir.x * speed, -jump_power)
 				else:
-					# jump straight up - costs stamina
 					var ratio = min(1.0, stamina / stamina_cost_wall_jump)
 					velocity.y = -jump_power * ratio
 					stamina = max(0, stamina - stamina_cost_wall_jump)
-			elif dir.y:
-				velocity.y = dir.y * speed * speed_mod * (0.5 if dir.y < 0 else 1.0)
+			elif im["up"] or im["down"]:
+				climbing_moving = true
+				velocity.y = (-speed * 0.8) if im["up"] else speed
 			else:
 				velocity.y = 0
 		else:
 			climbing = false
-			if im["jump"]:
-				pass
 
 	elif im["jump"] and (is_on_floor() or frames_since_on_floor <= coyote_time_limit):
 		velocity.y = -jump_power
-	elif im["down"] and is_on_floor():
+	elif im["down"] and is_on_floor() and not im["climb"]:
 		crouching = true
 	elif not $head/ShapeCast2D.is_colliding():
 		crouching = false
