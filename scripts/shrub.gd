786231c896735363298c270c16f2668a3cd3eb98
Fra <Francis.xun.su@gmail.com>
2026-04-04 09:02:43 +0800

    Recolored Shrub, and made its movement pixel perfect

--- scripts/shrub.gd
+++ scripts/shrub.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed := 80.0
 var gravity := 1100.0
 var direction := 1.0
+var move_timer := 0.0
 
 func _ready():
 	$AnimatedSprite2D.play("default")
@@ -17,5 +18,13 @@ func _physics_process(delta):
 		$"Floor Check".position.x *= -1
 		$AnimatedSprite2D.flip_h = direction < 0
 
-	velocity.x = speed * direction
+	move_timer += delta
+	var step_time = 4.0 / speed # how long each 4px step takes
+
+	if move_timer >= step_time:
+		move_timer -= step_time
+		position.x += 4.0 * direction
+		position = Vector2i(round(position / 4)) * 4
+
+	velocity.x = 0
 	move_and_slide()
