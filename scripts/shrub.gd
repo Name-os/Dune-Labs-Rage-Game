bcd179c51ddc8f5132a73619476a482e3a80306c
Fra <Francis.xun.su@gmail.com>
2026-04-04 09:21:23 +0800

    Added transition frames for Shrub turning

--- scripts/shrub.gd
+++ scripts/shrub.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 var gravity := 1100.0
 var direction := 1.0
 var move_timer := 0.0
+var turn_timer := 0.0
+var turning := false
+var turn_duration := 0.08 # how long the turn frame shows in seconds
 
 func _ready():
 	$AnimatedSprite2D.play("default")
@@ -12,19 +15,27 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if $"Wall Check".is_colliding() or not $"Floor Check".is_colliding():
+	if turning:
+		turn_timer -= delta
+		if turn_timer <= 0:
+			turning = false
+			$AnimatedSprite2D.play("default")
+	elif $"Wall Check".is_colliding() or not $"Floor Check".is_colliding():
 		direction *= -1.0
 		$"Wall Check".target_position.x *= -1
 		$"Floor Check".position.x *= -1
 		$AnimatedSprite2D.flip_h = direction < 0
+		turning = true
+		turn_timer = turn_duration
+		$AnimatedSprite2D.play("shrub turn")
 
-	move_timer += delta
-	var step_time = 4.0 / speed # how long each 4px step takes
-
-	if move_timer >= step_time:
-		move_timer -= step_time
-		position.x += 4.0 * direction
-		position = Vector2i(round(position / 4)) * 4
+	if not turning:
+		move_timer += delta
+		var step_time = 4.0 / speed
+		if move_timer >= step_time:
+			move_timer -= step_time
+			position.x += 4.0 * direction
+			position = Vector2i(round(position / 4)) * 4
 
 	velocity.x = 0
 	move_and_slide()
